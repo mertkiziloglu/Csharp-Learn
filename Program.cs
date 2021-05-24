18349abc78dd28c0257c7382308456da85352088
Mert kızıloğlu <53661914+mertkiziloglu@users.noreply.github.com>
2021-05-24 14:35:55 +0300

    Tecnical Question

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace TEcnicalQuestion
+{
+    class Program
+    {
+        public static void Main()
+        {
+            int x = 100;
+            int y = 20;
+ 
+            x = x + y;
+            y = x - y;
+            x = x - y;
+            
+            Console.WriteLine("After swapping: x = " + x + ", y = " + y);
+        }
+    }
+}
